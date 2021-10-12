d03d5da4c069dc26986d15247cc5ec49c25180ce
Emmay <emmayalam1@gmail.com>
2021-10-12 01:09:28 -0400

    added bubbleSort visualization

---
+++ src/scripts/algorithms/bubble.js
@@ -0,0 +1,36 @@
+async function bubble() {
+  const bar = document.querySelectorAll(".bar");          // using querySelectorAll to grab all of the bars in a NodeList
+
+  for (let i = 0; i < bar.length - 1; i++) {              // iterating through the length of the NodeList
+    for (let j = 0; j < (bar.length - i - 1); j++) {      // iterating through the length of the NodeList MINUS the sorted bars
+      bar[j].style.background = "pink";                   // setting the two bars being compared to a different color
+      bar[j + 1].style.background = "pink";
+
+      let bar1 = parseInt(bar[j].style.height);
+      let bar2 = parseInt(bar[j + 1].style.height);
+
+      if(bar1 > bar2) {
+        await delay(500);                                 // hard coding a value of 500ms for the delay between comparisons
+        swap(bar[j], bar[j + 1]);
+      };
+
+      bar[j].style.background = "cyan";                   // regardless of swap, setting compared bars back to original color
+      bar[j + 1].style.background = "cyan";
+    };
+
+    bar[bar.length - i - 1].style.background = "lightgreen";  // coloring bar bubbled up to the end to denote that it is sorted
+  };
+
+  bar[0].style.background = "lightgreen";                     // ensuring remaining bar after sorting is colored
+};
+
+function swap (bar1, bar2) {
+  let tempHeight = bar1.style.height;
+  bar1.style.height = bar2.style.height;
+  bar2.style.height = tempHeight;
+}
+
+const bubbleSortButton = document.querySelector("#bubble");
+bubbleSortButton.addEventListener("click", async function () {
+  await bubble();
+});
--- src/scripts/sort.js
+++ src/scripts/sort.js
@@ -1,46 +1,67 @@
-// generating a new array of data (currently set a min of 5 and max of 100 for data values)
+// generating a new array of data (currently set a min of 5 and max of 50 for data values) //
 
-function generateArray (amount = 25) {
+function generateArray (amount) {
+  deleteArray();
   const newArray = [];
 
   for (let i = 0; i < amount; i++) {
-    newArray.push(getRandomInt(5, 100))
+    newArray.push(getRandomInt(5, 50))
   };
 
   return newArray;
 };
 
-// rendering newly generated array
+// rendering newly generated array //
 
 function render (array) {
   const graph = document.querySelector(".graph");
 
   for (let i = 0; i < array.length; i++) {
     const bar = document.createElement("div");
+
     bar.classList.add("bar");
-    bar.style.height = `${array[i]}px`;
+    bar.style.height = `${array[i] * Math.PI}px`;
+
     graph.appendChild(bar);
   };
 };
 
-// delete previously generated / rendered array
+// delete previously generated / rendered array //
 
 function deleteArray () {
   const graph = document.querySelector(".graph");
-  graph.innerHTML = "";
-}
 
-// random number generator between a range (to provide random values for generated data) - from MDN docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  while (graph.firstChild) {
+    graph.removeChild(graph.firstChild);
+  };
+};
+
+
+// using promise function to add delay between sorting animations //
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+// random number generator between a range (to provide random values for generated data) - from MDN docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random //
 
 function getRandomInt (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
-// creating a button to generate new array of data
+// adding eventListener on button to generate new array of data //
 
 const generate = document.querySelector("#generate");
+
 generate.addEventListener("click", function () {
-  render(generateArray());
+  render(generateArray(parseInt(arraySize.value)));
+});
+
+// adding eventListener on slider to adjust array size of data //
+
+const arraySize = document.querySelector("#size");
+
+arraySize.addEventListener("input", function () {
+  render(generateArray(parseInt(arraySize.value)));
 });
