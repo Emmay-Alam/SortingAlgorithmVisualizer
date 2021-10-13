19e6294ef03a65e067c11314a88f14bb46722bb4
Emmay <emmayalam1@gmail.com>
2021-10-13 14:24:10 -0400

    updated sort.js script

--- src/scripts/sort.js
+++ src/scripts/sort.js
@@ -1,11 +1,11 @@
-// generating a new array of data (currently set a min of 5 and max of 50 for data values) //
+// generating a new array of data (currently set a min of 5 and max of 100 for data values) //
 
 function generateArray (amount) {
   deleteArray();
   const newArray = [];
 
   for (let i = 0; i < amount; i++) {
-    newArray.push(getRandomInt(5, 50))
+    newArray.push(getRandomInt(5, 100))
   };
 
   return newArray;
@@ -36,7 +36,6 @@ function deleteArray () {
   };
 };
 
-
 // using promise function to add delay between sorting animations //
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -56,7 +55,14 @@ const generate = document.querySelector("#generate");
 
 generate.addEventListener("click", function () {
   render(generateArray(parseInt(arraySize.value)));
+  enableButtons();                                              // enabling buttons here in case user doesn't want to see specific algorithm to the end
 });
+
+// changing color for sorted array //
+
+function sorted (bars) {
+  bars.forEach(el => el.style.background = "green");
+}
 
 // adding eventListener on slider to adjust array size of data //
 
@@ -65,3 +71,29 @@ const arraySize = document.querySelector("#size");
 arraySize.addEventListener("input", function () {
   render(generateArray(parseInt(arraySize.value)));
 });
+
+// updating text descriptions on algorithm event click //
+
+function updateDesc (desc) {
+  const title = document.querySelector(".description-title");
+  title.innerHTML = titles[`${desc}Title`];
+
+  const description = document.querySelector(".description")
+  description.innerHTML = descriptions[`${desc}Desc`];
+};
+
+// button disable function (meant for during sort) //
+
+function disableButtons () {
+  document.querySelector("#bubble").disabled = true;
+  document.querySelector("#merge").disabled = true;
+  document.querySelector("#quick").disabled = true;
+}
+
+// button enable function (meant for after sorting is complete) //
+
+function enableButtons () {
+  document.querySelector("#bubble").disabled = false;
+  document.querySelector("#merge").disabled = false;
+  document.querySelector("#quick").disabled = false;
+}
