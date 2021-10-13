74d256b3ace1e7faa275fa5afb0d374774a82f11
Emmay <emmayalam1@gmail.com>
2021-10-13 14:27:26 -0400

    updated bubble.js script

--- src/scripts/algorithms/bubble.js
+++ src/scripts/algorithms/bubble.js
@@ -1,32 +1,28 @@
-async function bubble() {
-  const bar = document.querySelectorAll(".bar");          // using querySelectorAll to grab all of the bars in a NodeList
+async function bubble (bars) {
+  for (let i = 0; i < bars.length - 1; i++) {              // iterating through the length of the NodeList
+    for (let j = 0; j < (bars.length - i - 1); j++) {      // iterating through the length of the NodeList MINUS the sorted bars
+      bars[j].style.background = "pink";                   // setting the two bars being compared to a different color
+      bars[j + 1].style.background = "pink";
 
-  for (let i = 0; i < bar.length - 1; i++) {              // iterating through the length of the NodeList
-    for (let j = 0; j < (bar.length - i - 1); j++) {      // iterating through the length of the NodeList MINUS the sorted bars
-      bar[j].style.background = "pink";                   // setting the two bars being compared to a different color
-      bar[j + 1].style.background = "pink";
-
-      let bar1 = parseInt(bar[j].style.height);
-      let bar2 = parseInt(bar[j + 1].style.height);
+      let bar1 = parseInt(bars[j].style.height);
+      let bar2 = parseInt(bars[j + 1].style.height);
 
       if(bar1 > bar2) {
-        await delay(500);                                 // hard coding a value of 500ms for the delay between comparisons
-        swap(bar[j], bar[j + 1]);
+        await delay(250);                                 // hard coding a value of 250ms for the delay between comparisons
+        swap(bars[j], bars[j + 1]);
       };
 
-      bar[j].style.background = "cyan";                   // regardless of swap, setting compared bars back to original color
-      bar[j + 1].style.background = "cyan";
+      bars[j].style.background = "cyan";                   // regardless of swap, setting compared bars back to original color
+      bars[j + 1].style.background = "cyan";
     };
 
-    bar[bar.length - i - 1].style.background = "lightgreen";  // coloring bar bubbled up to the end to denote that it is sorted
+    bars[bars.length - i - 1].style.background = "yellow";  // coloring bars bubbled up to the end to denote that it is sorted
   };
 
-  bar[0].style.background = "lightgreen";                     // ensuring remaining bar after sorting is colored
-
-  bar.forEach(el => el.style.background = "green");           // graph will be fully colored only when fully sorted
+  bars[0].style.background = "yellow";                     // ensuring remaining bar after sorting is colored
 };
 
-function swap (bar1, bar2) {
+function swap (bar1, bar2) {                                   // basic swap function to switch bar heights
   let tempHeight = bar1.style.height;
   bar1.style.height = bar2.style.height;
   bar2.style.height = tempHeight;
@@ -34,5 +30,10 @@ function swap (bar1, bar2) {
 
 const bubbleSortButton = document.querySelector("#bubble");
 bubbleSortButton.addEventListener("click", async function () {
-  await bubble();
+  updateDesc("bubble");                                         // updating text description for bubble sort
+  const bars = document.querySelectorAll(".bar");               // using querySelectorAll to grab all of the bars in a NodeList
+  disableButtons();
+  await bubble(bars);
+  sorted(bars);                                                 // changing final color of array to green
+  enableButtons();
 });
